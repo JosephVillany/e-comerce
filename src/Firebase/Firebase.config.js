404bf57aa5dd9c32966af268d9eb69ba18807b50
JosephVillany <josephvillany@gmail.com>
2021-11-30 22:28:39 -0500

    firebase

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,13 +4,21 @@ import 'firebase/compat/auth'
 import { getAuth, signOut } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlKiz7UT3X1fF-gTgdJQlD12iDFN3Lbas",
-  authDomain: "login-ecommerce-c8694.firebaseapp.com",
-  projectId: "login-ecommerce-c8694",
-  storageBucket: "login-ecommerce-c8694.appspot.com",
-  messagingSenderId: "717071805680",
-  appId: "1:717071805680:web:1beaf5d92d0d7dce546635",
-  measurementId: "G-94MM04JQ5D"
+  apiKey: "AIzaSyAjOMuuEVphRecQT2I1TiquFus4ME4J0Mk",
+  authDomain: "login-redux-academlo.firebaseapp.com",
+  projectId: "login-redux-academlo",
+  storageBucket: "login-redux-academlo.appspot.com",
+  messagingSenderId: "850493059107",
+  appId: "1:850493059107:web:481fad78c38a25bdb54196"
+
+
+  // apiKey: "AIzaSyAlKiz7UT3X1fF-gTgdJQlD12iDFN3Lbas",
+  // authDomain: "login-ecommerce-c8694.firebaseapp.com",
+  // projectId: "login-ecommerce-c8694",
+  // storageBucket: "login-ecommerce-c8694.appspot.com",
+  // messagingSenderId: "717071805680",
+  // appId: "1:717071805680:web:1beaf5d92d0d7dce546635",
+  //measurementId: "G-94MM04JQ5D"
 };
 
 firebase.initializeApp(firebaseConfig)
